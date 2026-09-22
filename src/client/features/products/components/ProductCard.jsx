89bfd7b72f5d6c60344d36c44930c8ae70292b4f
KingWep <thaneverlose168@gmail.com>
2026-09-22 08:17:14 +0700

    Update Price in card

--- src/client/features/products/components/ProductCard.jsx
+++ src/client/features/products/components/ProductCard.jsx
@@ -51,13 +51,11 @@ export default function ProductCard({ product = {}, index = 0 }) {
       : null;
 
   const discountPricePercent =
-    discountPrice ||
     (originalPrice
       ? Math.round(
           ((originalPrice - displayPrice) / originalPrice) * 100
         )
       : null);
-
   const savingsAmount = originalPrice
     ? originalPrice - displayPrice
     : cashback || 0;
